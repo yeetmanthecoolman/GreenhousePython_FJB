5d6a902c97b1084b6f902408a4c3b0eda4eac01a
yeetmanthecoolman <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-09 14:12:34 -0600

    Update dothing.sh
    I will see if I can make this work.

--- dothing.sh
+++ dothing.sh
@@ -1,4 +1,3 @@
 #the install, a basic outline
 sudo apt full-upgrade && sudo apt install gh git pipx #&& git stuff
-pipx ensurepath && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && poetry -vvv init#args go here 
-poetry -vvv install --all-groups --all-extras --compile
+pipx ensurepath && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && poetry -vvv install --all-groups --all-extras --compile && python3 main.py
