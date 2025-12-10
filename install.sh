64ed88a4a0f4d9a2893bc2a5f44c248c7cd063cc
yeetmanthecoolman <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-10 08:43:17 -0600

    Update install.sh
    Improved

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #the install command, a very basic outline
-sudo apt full-upgrade && sudo apt install gh git pipx #&& git stuff
-sudo pipx ensurepath -- global && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && eval $(poetry env activate) && poetry -vvv install --all-groups --all-extras --compile && poetry run python main.py 
+cd / && sudo apt full-upgrade && sudo apt install gh pipx && gh auth login && gh repo clone sp29174/GreenhousePython && cd ./GreenhousePython && sudo pipx ensurepath --global && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && eval $(poetry env activate) && poetry -vvv install --all-groups --all-extras --compile && poetry run python main.py 
+#Why have an entire installer when you can have a single very long command.
