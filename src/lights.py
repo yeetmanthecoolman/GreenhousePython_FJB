75764c98d0cca0959e998371d32ef1c6010f6c48
yeetmanthecoolman <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-10 10:54:38 -0600

    Revert lights.py
    This script needs to be able to manage variable durations.

--- src/lights.py
+++ src/lights.py
@@ -1,26 +1,27 @@
+#Yes, I reverted the changes. This script needs to be able to operate for variable lengths of time, and the revised script didn't do that. Furthermore, I'd like to do some testing on this script, because, again, I think I fixed it in the development before the initial commit.
+#TLDR, beautiful solution to the wrong problem.
 import datetime
 from datetime import timedelta, timezone, tzinfo
 from suntime import Sun, SunTimeException
 import RPi.GPIO as GPIO
 
-# Lighting GPIO (GPIO pinouts will probably have to change when we get things working)
+# Lighting GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 
 def lights(light_length):
   latitude = 43.0972
-  longitude = -89.5043
+  longitude = 89.5043
 
-  mcpasd = datetime.datetime.now() - timedelta(hours=6)
+  mcpasd = datetime.datetime.now(timezone.utc) - timedelta(hours=5)
 
   sun = Sun(latitude, longitude)
 
   light_on = False
 
   # Get today's sunrise and sunset in CST
-  today_sr = sun.get_sunrise_time() - timedelta(hours=6)
-  today_ss = sun.get_sunset_time() - timedelta(hours=6)
-  # unsure of use of this
+  today_sr = sun.get_sunrise_time() + timedelta(hours=7)
+  today_ss = sun.get_sunset_time() + timedelta(hours=7)
   if today_sr > mcpasd:
     today_sr = today_sr - timedelta(days=1)
   if today_sr > today_ss:
@@ -28,12 +29,12 @@ def lights(light_length):
     
   today_suntime = today_ss - today_sr
 
-  if(mcpasd > today_sr and mcpasd < today_ss):
-    light_on = False
-    return light_on
-  else:
+  light_on = today_suntime - today_suntime + timedelta(hours = light_length)
+
+  today_suntime = mcpasd - today_sr
+
+  if(mcpasd.time() > today_ss.time() and today_suntime < light_on):
     light_on = True
-    return light_on
+  else:
+    light_on = False
   GPIO.output(21, True)
-
-
