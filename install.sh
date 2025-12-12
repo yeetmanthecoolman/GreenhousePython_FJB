5b841186b10a8f18c8ef4e2b33830fbce971fd74
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-12 09:53:03 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #the install command, a very basic outline
-cd / && sudo apt full-upgrade && sudo apt install git pipx gcc libcap-dev python3-dev && sudo git clone https://github.com/sp29174/GreenhousePython.git && sudo pipx ensurepath && sudo reboot
-cd / && sudo pipx install poetry && sudo pipx upgrade poetry && sudo poetry completions bash >> /root/.bash_completion && cd ./GreenhousePython && eval $(sudo poetry env activate) && sudo poetry install -vvv --all-groups --all-extras --compile && sudo poetry run python ./src/main.py 
+cd / && sudo apt full-upgrade && sudo apt install git pipx gcc libcap-dev python3-dev && sudo git clone https://github.com/sp29174/GreenhousePython.git && sudo chmod -R 777 /GreenhousePython && sudo pipx ensurepath && sudo reboot
+cd / && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && cd ./GreenhousePython && eval $(poetry env activate) && poetry install -vvv --all-groups --all-extras --compile && poetry run python ./src/main.py 
 #Why have an entire installer when you can have two very long commands.
