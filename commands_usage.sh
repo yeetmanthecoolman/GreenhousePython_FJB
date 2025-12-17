d55720d68a114e9484f88d5fb701d74f209d6702
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-17 14:39:17 -0600

    Update commands_usage.sh

--- commands_usage.sh
+++ commands_usage.sh
@@ -1,7 +1,7 @@
 #To install, please run these three commands in sequence. We provide no gaurentuee that these commands won't break your system.
 sudo do-release-upgrade -d
 cd ~/ && sudo apt full-upgrade && sudo apt install g++ libkms++-dev libatlas-base-dev ffmpeg libopenjp2-7 libcamera-dev libfmt-dev libdrm-dev git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev python3-tk && git clone https://github.com/sp29174/GreenhousePython.git && pipx ensurepath && reboot
-cd ~/ && pipx install -vvv poetry && poetry completions bash >> ~/.bash_completion && cd ./GreenhousePython && eval $(poetry env activate) && poetry install -vvv --all-groups --all-extras --compile && poetry run -vvv python -vvi ./src/main.py 
+cd ~/ && pipx install -vvv poetry && cd ./GreenhousePython && eval $(poetry env activate) && poetry install -vvv --all-groups --all-extras --compile && poetry run -vvv python -vvi ./src/main.py 
 #To run the program, please run this command:
 cd ~/ && sudo apt full-upgrade && cd ./GreenhousePython && poetry update -vvv && poetry run -vvv python -vvi ./src/main.py
 #To uninstall, please run this command:
