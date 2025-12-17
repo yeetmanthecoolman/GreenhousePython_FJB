25651e7654eb8fb312c7e54b25b97454653291b4
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-17 09:46:47 -0600

    Update commands_usage.sh

--- commands_usage.sh
+++ commands_usage.sh
@@ -1,5 +1,5 @@
 #To install, please run these two commands in sequence. The first command requires SUDO and will will restart your computer. The second command is benign.
-cd ~/ && sudo apt full-upgrade && sudo apt install libatlas-base-dev ffmpeg libopenjp2-7 libcamera-dev libfmt-dev libdrm-dev git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev python3-tk && git clone https://github.com/sp29174/GreenhousePython.git && pipx ensurepath && reboot
+cd ~/ && sudo apt full-upgrade && sudo apt install g++ libatlas-base-dev ffmpeg libopenjp2-7 libcamera-dev libfmt-dev libdrm-dev git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev python3-tk && git clone https://github.com/sp29174/GreenhousePython.git && pipx ensurepath && reboot
 cd ~/ && pipx install -vvv poetry && poetry completions bash >> ~/.bash_completion && cd ./GreenhousePython && eval $(poetry env activate) && poetry install -vvv --all-groups --all-extras --compile && poetry run -vvv python -vvi ./src/main.py 
 #To run the program, please run this command:
 cd ~/ && sudo apt full-upgrade && cd ./GreenhousePython && poetry update -vvv && poetry run -vvv python -vvi ./src/main.py
