536e1af2f6a226f3ab1fc00d951a8b5516b5442c
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-13 14:52:41 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -33,102 +33,6 @@ dt = 200
 lightPin = 21
 waterPin = 16
 MAX_VALUE = 65535
-	
-# GUI ****************************************************************************************	
-
-# window
-window = tk.Tk()
-window.title =('Greenhouse')
-window.geometry(resolution)#This needs to be changed
-
-# title
-title_label = ttk.Label(master = window, text = 'Greenhouse', font = header_font)
-title_label.pack()
-
-#first layer
-layer1_frame = ttk.Frame(master = window)
-
-# image information
-image_frame = ttk.Frame(master = layer1_frame)
-image = Image.open(lastFileName())
-image2 = image.resize((640, 480))
-last_plant_image = ImageTk.PhotoImage(image2)
-image_label = ttk.Label(master = image_frame, image = last_plant_image)
-
-image_label_frame = ttk.Frame(master = image_frame)
-interval_label = ttk.Label(master = image_label_frame, text = 'Interval is set to ', font = norm_font)
-capture_label = ttk.Label(master = image_label_frame, text = 'There have been __ captures\nsince last time-lapse.', font = norm_font)
-
-# packing image stuff
-image_label.pack(side = 'left', padx = 10, pady = 10)
-interval_label.pack(padx = 5, pady = 30)
-capture_label.pack(padx = 5, pady = 5)
-image_label_frame.pack(side = 'left', padx = 10, pady = 10)
-image_frame.pack(side = 'left')
-
-# far right info
-top_right_frame = ttk.Frame(master = layer1_frame)
-last_capture = ttk.Label(master = top_right_frame, text = 'Last capture was taken ___ minutes ago.', font = norm_font)
-zone_frame = ttk.Frame(master = top_right_frame)
-zone_label = ttk.Label(master = zone_frame, text = "Zone Moistures", font = norm_font)
-bzone1 = ttk.Button(master = zone_frame, text = "Left Bed: " + str(get_data(0)))#These only update once
-bzone2 = ttk.Button(master = zone_frame, text = "Middle Bed: " + str(get_data(1)))
-bzone3 = ttk.Button(master = zone_frame, text = "Right Bed: " + str(get_data(2)))
-
-moisture_frame = ttk.Frame(master = top_right_frame)
-moisture_label = ttk.Label(master = moisture_frame, text = "Select Moisture Level", font = norm_font)
-top_buttons = ttk.Frame(master = moisture_frame)
-bottom_buttons = ttk.Frame(master = moisture_frame)
-bmoisture0 = ttk.Button(master = top_buttons, text = "0%", command = lambda : water(0))#these should not be hardcoded
-bmoisture1 = ttk.Button(master = top_buttons, text = "20%", command = lambda : water(0.2))
-bmoisture2 = ttk.Button(master = top_buttons, text = "40%", command = lambda : water(0.4))
-bmoisture3 = ttk.Button(master = bottom_buttons, text = "60%", command = lambda : water(0.6))
-bmoisture4 = ttk.Button(master = bottom_buttons, text = "80%", command = lambda : water(0.8))
-bmoisture5 = ttk.Button(master = bottom_buttons, text = "100%", command = lambda : water(1))
-
-# far right packing
-last_capture.pack(padx = 10, pady = 20)
-zone_label.pack(padx = 10, pady = 20)
-bzone1.pack(side = 'left', padx = 5, pady = 5)
-bzone2.pack(side = 'left', padx = 5, pady = 5)
-bzone3.pack(side = 'left', padx = 5, pady = 5)
-zone_frame.pack(padx = 10, pady = 10)
-
-moisture_label.pack(padx = 5, pady = 20)
-bmoisture0.pack(side = 'left', padx = 5, pady = 5)
-bmoisture1.pack(side = 'left', padx = 5, pady = 5)
-bmoisture2.pack(padx = 5, pady = 5)
-top_buttons.pack(padx = 5, pady = 5)
-bmoisture3.pack(side = 'left', padx = 5, pady = 5)
-bmoisture4.pack(side = 'left', padx = 5, pady = 5)
-bmoisture5.pack(padx = 5, pady = 5)
-bottom_buttons.pack(padx = 5, pady = 5)
-moisture_frame.pack(padx = 10, pady = 10)
-
-top_right_frame.pack(padx=10, pady=10)
-layer1_frame.pack(padx = 20, pady = 20)
-
-# lower layer
-layer2_frame = ttk.Frame(master = window)
-
-# captures picture, command= cameraCapture
-# ISSUE: taking picture on boot
-#I disagree, that's a feature!
-manual_pic_button = ttk.Button(master = layer2_frame, text = "Take Manual\nPicture", command = lambda : image_update(attrs,theCamera))
-
-# should start recording function
-start_record = ttk.Button(master = layer2_frame, text = recording_status)
-light_label = ttk.Label(master = layer2_frame, text = "Enter the number of hours the selected\ngrowlight should remain on.\nCurrently " + str(light_length) + " hours per day.", font = norm_font)
-light_cycle = ttk.Entry(master = layer2_frame)
-enter_button = ttk.Button(master = layer2_frame, text = "Enter Hours", command = new_light_control)
-
-#packing lower layer
-manual_pic_button.pack(side = 'left', padx = 25, pady = 5)
-start_record.pack(side = 'left', padx = 25, pady = 5)
-light_label.pack(padx = 25, pady = 5)
-light_cycle.pack(padx = 25, pady = 5)
-enter_button.pack(padx = 25, pady = 5)
-layer2_frame.pack(padx = 5, pady = 5)
 
 # methods   ***********************************************************************************
 
@@ -282,6 +186,102 @@ def compare(num):
 		if(num > 1.2 * x or num < 0.8 * x):
 			return False
 	return True
+	
+# GUI ****************************************************************************************	
+
+# window
+window = tk.Tk()
+window.title =('Greenhouse')
+window.geometry(resolution)#This needs to be changed
+
+# title
+title_label = ttk.Label(master = window, text = 'Greenhouse', font = header_font)
+title_label.pack()
+
+#first layer
+layer1_frame = ttk.Frame(master = window)
+
+# image information
+image_frame = ttk.Frame(master = layer1_frame)
+image = Image.open(lastFileName())
+image2 = image.resize((640, 480))
+last_plant_image = ImageTk.PhotoImage(image2)
+image_label = ttk.Label(master = image_frame, image = last_plant_image)
+
+image_label_frame = ttk.Frame(master = image_frame)
+interval_label = ttk.Label(master = image_label_frame, text = 'Interval is set to ', font = norm_font)
+capture_label = ttk.Label(master = image_label_frame, text = 'There have been __ captures\nsince last time-lapse.', font = norm_font)
+
+# packing image stuff
+image_label.pack(side = 'left', padx = 10, pady = 10)
+interval_label.pack(padx = 5, pady = 30)
+capture_label.pack(padx = 5, pady = 5)
+image_label_frame.pack(side = 'left', padx = 10, pady = 10)
+image_frame.pack(side = 'left')
+
+# far right info
+top_right_frame = ttk.Frame(master = layer1_frame)
+last_capture = ttk.Label(master = top_right_frame, text = 'Last capture was taken ___ minutes ago.', font = norm_font)
+zone_frame = ttk.Frame(master = top_right_frame)
+zone_label = ttk.Label(master = zone_frame, text = "Zone Moistures", font = norm_font)
+bzone1 = ttk.Button(master = zone_frame, text = "Left Bed: " + str(get_data(0)))#These only update once
+bzone2 = ttk.Button(master = zone_frame, text = "Middle Bed: " + str(get_data(1)))
+bzone3 = ttk.Button(master = zone_frame, text = "Right Bed: " + str(get_data(2)))
+
+moisture_frame = ttk.Frame(master = top_right_frame)
+moisture_label = ttk.Label(master = moisture_frame, text = "Select Moisture Level", font = norm_font)
+top_buttons = ttk.Frame(master = moisture_frame)
+bottom_buttons = ttk.Frame(master = moisture_frame)
+bmoisture0 = ttk.Button(master = top_buttons, text = "0%", command = lambda : water(0))#these should not be hardcoded
+bmoisture1 = ttk.Button(master = top_buttons, text = "20%", command = lambda : water(0.2))
+bmoisture2 = ttk.Button(master = top_buttons, text = "40%", command = lambda : water(0.4))
+bmoisture3 = ttk.Button(master = bottom_buttons, text = "60%", command = lambda : water(0.6))
+bmoisture4 = ttk.Button(master = bottom_buttons, text = "80%", command = lambda : water(0.8))
+bmoisture5 = ttk.Button(master = bottom_buttons, text = "100%", command = lambda : water(1))
+
+# far right packing
+last_capture.pack(padx = 10, pady = 20)
+zone_label.pack(padx = 10, pady = 20)
+bzone1.pack(side = 'left', padx = 5, pady = 5)
+bzone2.pack(side = 'left', padx = 5, pady = 5)
+bzone3.pack(side = 'left', padx = 5, pady = 5)
+zone_frame.pack(padx = 10, pady = 10)
+
+moisture_label.pack(padx = 5, pady = 20)
+bmoisture0.pack(side = 'left', padx = 5, pady = 5)
+bmoisture1.pack(side = 'left', padx = 5, pady = 5)
+bmoisture2.pack(padx = 5, pady = 5)
+top_buttons.pack(padx = 5, pady = 5)
+bmoisture3.pack(side = 'left', padx = 5, pady = 5)
+bmoisture4.pack(side = 'left', padx = 5, pady = 5)
+bmoisture5.pack(padx = 5, pady = 5)
+bottom_buttons.pack(padx = 5, pady = 5)
+moisture_frame.pack(padx = 10, pady = 10)
+
+top_right_frame.pack(padx=10, pady=10)
+layer1_frame.pack(padx = 20, pady = 20)
+
+# lower layer
+layer2_frame = ttk.Frame(master = window)
+
+# captures picture, command= cameraCapture
+# ISSUE: taking picture on boot
+#I disagree, that's a feature!
+manual_pic_button = ttk.Button(master = layer2_frame, text = "Take Manual\nPicture", command = lambda : image_update(attrs,theCamera))
+
+# should start recording function
+start_record = ttk.Button(master = layer2_frame, text = recording_status)
+light_label = ttk.Label(master = layer2_frame, text = "Enter the number of hours the selected\ngrowlight should remain on.\nCurrently " + str(light_length) + " hours per day.", font = norm_font)
+light_cycle = ttk.Entry(master = layer2_frame)
+enter_button = ttk.Button(master = layer2_frame, text = "Enter Hours", command = new_light_control)
+
+#packing lower layer
+manual_pic_button.pack(side = 'left', padx = 25, pady = 5)
+start_record.pack(side = 'left', padx = 25, pady = 5)
+light_label.pack(padx = 25, pady = 5)
+light_cycle.pack(padx = 25, pady = 5)
+enter_button.pack(padx = 25, pady = 5)
+layer2_frame.pack(padx = 5, pady = 5)
 
 
 # startup ****************************************************************************************
@@ -308,6 +308,7 @@ camera_cfg = picam2.create_still_configuration()
 theCamera.start()
 window.after(dt, lambda : repeater(dt,latitude,longitude))
 window.mainloop()
+
 
 
 
