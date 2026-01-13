2b5050f1eb3fedf5d49b0eb4d6ee0ec27a51b030
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-13 10:46:50 -0600

    Update commands_usage.sh

--- commands_usage.sh
+++ commands_usage.sh
@@ -1,11 +1,11 @@
 #To install, please run these three commands in sequence. We provide no gaurentuee that these commands won't break your system.
 sudo do-release-upgrade -d
 cd ~/ && sudo apt install --install-recommends g++ libkms++-dev pkgconf ffmpeg libopenjp2-7 libcamera-dev libfmt-dev libdrm-dev git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev python3-tk && git clone https://github.com/sp29174/GreenhousePython.git && pipx ensurepath && reboot
-cd ~/GreenhousePython && pipx install -vvv poetry && cd ./GreenhousePython && poetry install -vvv --all-groups --all-extras --compile && poetry run -vvv python -vvi ./src/main.py 
+cd ~/GreenhousePython && pipx install -vvv poetry && poetry install -vvv --all-groups --all-extras --compile && poetry run -vvv python -vvi ./src/main.py 
 #To run the program, please run this command:
-cd ~/GreenhousePython && sudo apt full-upgrade && cd ./GreenhousePython && poetry update -vvv && poetry run -vvv python -vvi ./src/main.py
+cd ~/ && sudo apt full-upgrade && cd ./GreenhousePython && poetry update -vvv && poetry run -vvv python -vvi ./src/main.py
 #To uninstall, please run this command:
-cd ~/GreenhousePython && poetry env remove --all && pipx uninstall-all -vvv && rm -rf ./GreenhousePython && sudo apt purge git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev && sudo apt full-upgrade && reboot
+cd ~/ && poetry env remove --all && pipx uninstall-all -vvv && rm -rf ./GreenhousePython && sudo apt purge git pipx gcc gcc-aarch64-linux-gnu libcap-dev python3-dev && sudo apt full-upgrade && reboot
 
 #NOTE that we define "the uninstaller works" to be true iff running $2, $3, $4, and $8 produces *precisely the same disk* as running sudo do-release-upgrade -d, then running sudo apt full-upgrade.
 #NOTE that the uninstaller does not work.
