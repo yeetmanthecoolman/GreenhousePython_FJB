f36b2879fc7670f1723a781a7131da029f76e62e
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-15 10:44:06 -0600

    Update nonsense.py

--- src/greenhousepython/nonsense.py
+++ src/greenhousepython/nonsense.py
@@ -5,3 +5,15 @@ class Picamera2:
     pass
   def create_still_configuration(self):
     pass
+
+class GPIO:
+  BCM = None
+  OUT = None
+  HIGH = None
+  LOW = None
+  def setmode(thing):
+    pass
+  def setup(pin,mode):
+    pass
+  def output(pin,thing):
+    pass
