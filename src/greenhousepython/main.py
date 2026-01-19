c2ae5d81c61b4b25680495b6e8354d280062212c
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-18 22:21:09 -0600

    Update main.py
    Fix bovine mistake

--- src/greenhousepython/main.py
+++ src/greenhousepython/main.py
@@ -125,7 +125,7 @@ def water(input : float = None):
 		moisture = get_data(x)
 		if (attrs["bed" + str(x)] == "True") and (int(attrs["MAX_VALUE"]) * (float(attrs["control_parameter"]) - float(attrs["deadband"])) < moisture):
 			GPIO.output(int(attrs["waterPin"]), GPIO.HIGH)#replace with whatever turns on bed x
-			attrs["bed" + str(x)] = "False"
+			attrs["bed" + str(x)] = "True"
 			setAttributes()
 		elif (attrs["bed" + str(x)] == "False") and (int(attrs["MAX_VALUE"]) * (float(attrs["control_parameter"]) + float(attrs["deadband"])) > moisture):
 			GPIO.output(int(attrs["waterPin"]), GPIO.LOW)#replace with whatever turns off bed x
@@ -331,6 +331,7 @@ def start_gui():
 
 # Finalization and execution ****************************************************************************************
 app()
+
 
 
 
