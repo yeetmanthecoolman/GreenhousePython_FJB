d25e5921609a54c64867a96cbd01203e243c20b0
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-02-20 10:36:16 -0600

    Update main.py
    Update for migration to Shelves

--- src/greenhousepython/main.py
+++ src/greenhousepython/main.py
@@ -5,30 +5,40 @@
 #Preinitialization ****************************************************************************************
 
 from typer import Typer, Argument, Option
+import shelve
 app = Typer(rich_markup_mode="rich")
-attrs = {}
+attrs = shelve.open("cfg.txt", writeback = True)
+if not attrs:
+	attrs["last_file_number"] = 0
+	attrs["camera_interval"] = 3600
+	attrr["interval"] = 1
+	attrs["file_name_prefix"] = "gi"
+	attrs["lights"] = 1
+	attrs["light_length"] = 0.5
+	attrs["latitude"] = 43.0972
+	attrs["longitude"] = -89.5043
+	attrs["elevation"] = 355.0
+	attrs["light_pin0"] = 21
+	attrs["pump_pin"] = 15
+	attrs["water_pin0"] = 16
+	attrs["water_pin1"] = 17
+	attrs["water_pin2"] = 18
+	attrs["is_debug"] = True
+	attrs["is_recording"] = True
+	attrs["beds"] = 3
+	attrs["bed0"] = False
+	attrs["bed1"] = False
+	attrs["bed2"] = False
+	attrs["control_parameter0"] = 0.0
+	attrs["deadband0"] = 0.0
+	attrs["control_parameter1"] = 0.0
+	attrs["deadband1"] = 0.0
+	attrs["control_parameter2"] = 0.0
+	attrs["deadband2"] = 0.0
+	attrs.sync()
+	#create default attrs
 
 #Helpers ****************************************************************************************
-
-#read configuration information from cfg.txt and use it
-def get_attributes():
-	global attrs
-	cfg = open("cfg.txt", "r")
-	for thing in cfg.readlines():#find all things seperated by newlines
-		kvp = thing.split(":")#get key-value pairs
-		attrs[kvp[0]] = kvp[1].rstrip("\n")#add key-value pair to dictionary, no random trailing newline for you
-	cfg.close()
-
-#rewrite the list with updated values
-def set_attributes():
-	global attrs
-	cfg = open("cfg.txt", "w")#open file to write
-	accumulator = []
-	keys = attrs.keys()#get all the keys
-	for key in keys:#Conveniently, we only refer to these using their names, which means it doesn't  matter if this reorganizes our cfg constantly. It was never even well-ordered to begin with!
-		accumulator.append(key + ":" + attrs[key] + "\n")#assemble key and values into new format
-	cfg.writelines(accumulator)#append to file
-	cfg.close()
 
 #figure out where the nth photo is (or at least should be)
 def get_file_name(file_number):
@@ -39,7 +49,6 @@ def get_file_name(file_number):
 
 # Initialization ****************************************************************************************
 
-get_attributes()
 has_GUI = True
 try:
 	import cv2
@@ -50,19 +59,19 @@ try:
 	import mcp3008 as MCP
 	from mcp3008 import MCP3008
 except ImportError as e:
-	if attrs["is_debug"] == "True":
+	if attrs["is_debug"]:
 		print("WARNING: " + str(e))
 	from greenhousepython.nonsense import MCP, MCP3008
 try:
 	from greenhousepython.gpio_wrapper import gpio as g
 	GPIO = g()
 except Exception as e:
-	if attrs["is_debug"] == "True":
+	if attrs["is_debug"]:
 		print("WARNING: " + str(e))
 	try:
 		import RPi.GPIO as GPIO
 	except ImportError as e:
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		from greenhousepython.nonsense import GPIO
 import sys
@@ -73,7 +82,7 @@ try:
 	from gi.repository import GLib, Gtk
 	from gi.events import GLibEventLoopPolicy
 except Exception as e:
-	if attrs["is_debug"] == "True":
+	if attrs["is_debug"]:
 		print("WARNING: " + str(e))
 	has_GUI = False
 from datetime import datetime, timedelta, timezone
@@ -84,17 +93,17 @@ from typing import Annotated
 # Postinitialization ****************************************************************************************
 
 times_off = []
-for n in range(int(attrs["lights"])):
+for n in range(attrs["lights"]):
 	times_off.append(datetime.now(timezone.utc))
 # create the mcp object
 mcp = MCP3008.fixed([MCP.CH0, MCP.CH1, MCP.CH2, MCP.CH3, MCP.CH4, MCP.CH5, MCP.CH6, MCP.CH7])
 # setup other GPIO
 GPIO.setmode(GPIO.BCM)
-for x in range(int(attrs["lights"])):
-	GPIO.setup(int(attrs["light_pin" + str(x)]), GPIO.OUT)
-for x in range(int(attrs["beds"])):
-	GPIO.setup(int(attrs["water_pin" + str(x)]), GPIO.OUT)
-GPIO.setup(int(attrs["pump_pin"]), GPIO.OUT)
+for x in range(attrs["lights"]):
+	GPIO.setup(attrs["light_pin" + str(x)], GPIO.OUT)
+for x in range(attrs["beds"]):
+	GPIO.setup(attrs["water_pin" + str(x)], GPIO.OUT)
+GPIO.setup(attrs["pump_pin"], GPIO.OUT)
 the_camera = cv2.VideoCapture(0)
 
 # More helpers ***********************************************************************************
@@ -107,24 +116,25 @@ def do_shutdown(*args,**kwargs):
 	try:#we shouldn't let crashes prevent the program from closing, so these must all be wrapped with try.
 		mcp.close()#close down water control coms
 	except Exception as e:
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't close down the water control communitcations.")
 	try:
 		the_camera.release()#turn off the damn camera
 	except Exception as e:
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't close down the camera.")
 	try:
 		GPIO.cleanup()#knock the GPIO back to high-zed
 	except Exception as e:
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't reset the GPIO.")
+	attrs.close()
 	sys.exit(0)#ensure the proghramme actually ends
 
 # Postpostinitialization ***********************************************************************************
@@ -136,11 +146,11 @@ signal.signal(signal.SIGINT,do_shutdown)#I have no idea why these aren't the sam
 
 #a simple command to allow the user to change settings-this lets them define nonsense parameters, but I could care less, because my get_attributes can ignore them.
 #However, this almost certainly breaks if you pass in a thing that contains a newline, which we should fix later.
-@app.command(help="Change the setting KEY to VALUE. Newlines and colons are not supported for technical reasons.")
+@app.command(help="Change the setting KEY to VALUE. Newlines and colons are not supported for technical reasons. BROKEN while we switch to Shelves.")
 def change_setting(key : Annotated[str, Argument(help="The exact name of the setting to change or create.")], value : Annotated[str, Argument(help="The exact value that the setting should be changed to.")]):
 	global attrs
 	attrs[key] = value
-	set_attributes()
+	attrs.sync()
 
 #control pumps using hysteresis based on the values returned from the MCP
 @app.command(help="Force the system to run the automatic water control logic without starting the GUI.")
@@ -148,16 +158,16 @@ def water():
 	global attrs
 	run_pump = False
 	try:#my crummy mock of mcp3008.MCP3008 can't make sense of line 118, so this eyesore is trapped in a try-catch.
-		for x in range(int(attrs["beds"])):
+		for x in range(attrs["beds"]):
 			moisture = mcp(1)[x]#normalize values to one using mcp3008.MCP3008 (see https://github.com/luxedo/RPi_mcp3008/blob/master/mcp3008.py#L73)
-			if (attrs["bed" + str(x)] == "False") and (moisture < float(attrs["control_parameter" + str(x)]) - (float(attrs["deadband" + str(x)])/2)):#this if-else is basically an inelegant hysteresis controller. On the other hand, we're being rewarded for not destroying the pump, not for elegantly destroying the pump.
+			if (not attrs["bed" + str(x)]) and (moisture < attrs["control_parameter" + str(x)] - (attrs["deadband" + str(x)]/2)):#this if-else is basically an inelegant hysteresis controller. On the other hand, we're being rewarded for not destroying the pump, not for elegantly destroying the pump.
 				GPIO.output(int(attrs["water_pin" + str(x)]), GPIO.HIGH)
 				attrs["bed" + str(x)] = "True"
-				set_attributes()
-			elif (attrs["bed" + str(x)] == "True") and (moisture > float(attrs["control_parameter" + str(x)]) + (float(attrs["deadband" + str(x)])/2)):
-				GPIO.output(int(attrs["water_pin" + str(x)]), GPIO.LOW)
-				attrs["bed" + str(x)] = "False"
-				set_attributes()
+				attrs.sync()
+			elif attrs["bed" + str(x)] and (moisture > attrs["control_parameter" + str(x)] + (attrs["deadband" + str(x)]/2)):
+				GPIO.output(attrs["water_pin" + str(x)], GPIO.LOW)
+				attrs["bed" + str(x)] = False
+				attrs.sync()
 			if (attrs["bed" + str(x)] == "True"):
 				run_pump = True#If any bed is on, then run the pump.
 		if run_pump:
@@ -165,7 +175,7 @@ def water():
 		else:
 			GPIO.output(int(attrs["pump_pin"]), GPIO.LOW)
 	except Exception as e:
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("We failed at water control. This is probably because we aren't connected to an MCP3008, which is reasonable. If it isn't reasonable, check the dependencies.")
@@ -183,32 +193,32 @@ def light():#This code is a disaster area. Essentially, here's the logic:
 	#There must be a better solution than this, but I couldn't find it. Shrug emoji.
 	global attrs
 	global times_off
-	observer = Observer(float(attrs["latitude"]),float(attrs["longitude"]),float(attrs["elevation"]))
+	observer = Observer(attrs["latitude"],attrs["longitude"],attrs["elevation"])
 	try:
 		the_sun = sun.daylight(observer)#The sun is a deadly laser	
 	except ValueError as e:#the sun doesn't rise/set today. We therefore need to fudge the sun. This means that, for our purposes, the sun rose at midnight and set a very long time ago.
-		if attrs["is_debug"] == True:
+		if attrs["is_debug"]:
 			print(str(e))
 		the_sun = [datetime(datetime.year,datetime.month,datetime.day),datetime.min]
 	if (datetime.now(timezone.utc) >= the_sun[1]):
-		if attrs["is_debug"] == "True":
+		if attrs["is_debug"]:
 			print("We think that it's after sunset.")
-		for n in range(int(attrs["lights"])):
-			times_off[n] = the_sun[0] + timedelta(days=float(attrs["light_length" + str(n)]))
-	elif attrs["is_debug"] == "True":
+		for n in range(attrs["lights"]):
+			times_off[n] = the_sun[0] + timedelta(days=attrs["light_length" + str(n)])
+	elif attrs["is_debug"]:
 		print("We think that it's before sunset.")
-	for n in range(int(attrs["lights"])):
+	for n in range(attrs["lights"]):
 		light_on = False
 		if (datetime.now(timezone.utc) < times_off[n]):
 			light_on = True
-			if attrs["is_debug"] == "True":
+			if attrs["is_debug"]:
 				print("The light should be on.")
-		elif attrs["is_debug"] == "True":
+		elif attrs["is_debug"]:
 			print("The light should be off.")
 		if light_on:
-			GPIO.output(int(attrs["light_pin" + str(n)]), GPIO.HIGH)
+			GPIO.output(attrs["light_pin" + str(n)], GPIO.HIGH)
 		else:
-			GPIO.output(int(attrs["light_pin" + str(n)]), GPIO.LOW)
+			GPIO.output(attrs["light_pin" + str(n)], GPIO.LOW)
 
 #A command that captures a photograph, writes it to a file, and updates attrs accordingly.
 @app.command(help="Manually capture a photograph.")
@@ -217,19 +227,19 @@ def camera_capture():#updated to not badly reimplement last_file_name
 	global the_camera
 	ret, frame = the_camera.read()
 	if not ret:
-		if not attrs["is_debug"] == "True":
-			assert False
+		if not attrs["is_debug"]:
+			assert False#Fuck this!
 		else:
 			print("Warning: Image capture failed to complete.")
 	else:
-		cv2.imwrite(get_file_name(int(attrs["last_file_number"]) + 1),frame)
-		attrs["last_file_number"] = str(int(attrs["last_file_number"]) + 1)
-		set_attributes()
+		cv2.imwrite(get_file_name(attrs["last_file_number"] + 1, frame)
+		attrs["last_file_number"] = attrs["last_file_number"] + 1
+		attrs.sync()
 
 @app.command(help="Assemble the current collection of images into a mp4 video. This will:\n \n 1. Not include the placeholder image, ever. \n 2. Not recoginze images created with file numbers above last_file_number. \n 3. Pollute your shell with warnings if you have nonsequential image numbers. \n 4. Be unaware of the fact that manually captured images are not taken at regular intervals.")
 def create_video(output_video_path : Annotated[str, Argument(help="An exact file name, which must be writable, must not have stuff in it, and should not be irrational. If you run this command with sudo and pass /, then it will replace your system with a video. Don't.")], fps : Annotated[int, Option(help="The framerate that the video will be displayed at. Defaults to 24.")] = 24, size : Annotated[str, Option(help="An override to resize your images to a different resolution. This is needed internally for technical reasons, and we saw no reason not to expose it to the commandline. Defaults to your camera resolution.")] = None):
 	image_paths = []
-	for num in range(1,int(attrs["last_file_number"])+1):
+	for num in range(1,attrs["last_file_number"] + 1):
 		if cv2.imread(get_file_name(num)) is not None:
 			image_paths.append(get_file_name(num))
 		else:
@@ -279,13 +289,13 @@ class GUI:
 		self.loop = self.policy.get_event_loop()
 		self.tasks = []
 		self.gui_app = Gtk.Application(application_id="com.github.sp29174.GreenhousePython")
-		if attrs["is_debug"]=="True":
+		if attrs["is_debug"]:
 			print("we have super")
 		self.gui_app.connect("activate",self.do_activate)
-		if attrs["is_debug"]=="True":
+		if attrs["is_debug"]:
 			print("we can bind")
 		self.gui_app.run(None)
-		if attrs["is_debug"]=="True":
+		if attrs["is_debug"]:
 			print("we get to the bloody twilight zone")
 		do_shutdown()
 	def do_activate(self,useless):
@@ -295,7 +305,7 @@ class GUI:
 		self.window.set_child(self.notebook)
 		#stuff goes here
 		self.camera_page = Gtk.CenterBox()
-		self.preview_image = Gtk.Image.new_from_file(get_file_name(int(attrs["last_file_number"])))
+		self.preview_image = Gtk.Image.new_from_file(get_file_name(attrs["last_file_number"]))
 		self.preview_image.set_vexpand(True)#Allow the preview image to not be two pixels tall
 		self.preview_image.set_hexpand(True)
 		self.camera_page.set_start_widget(self.preview_image)
@@ -306,7 +316,7 @@ class GUI:
 		self.capture_button.connect("clicked", lambda button: self.tasks.append(self.loop.create_task(self.force_capture())))
 		self.capture_box.append(self.capture_button)
 		self.record_button = Gtk.ToggleButton(label="Toggle recording.")
-		if attrs["recording_status"] == "True":
+		if attrs["is_recording"]:
 			self.record_button.set_active(True)
 		else:
 			self.record_button.set_active(False)
@@ -322,13 +332,13 @@ class GUI:
 			self.water_pages.append(Gtk.CenterBox())
 			self.water_pages[n].set_start_widget(Gtk.Label(label="This text should vanish before you can read it."))#Namely, line 312 should cause automatic_control to await update_GUI which should disappear this placeholder. If anything crashes between here and line 402, this text will live and we learn of a problem.
 			self.water_scales.append(Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL,0,1,0.01))
-			self.water_scales[n].set_value(float(attrs["control_parameter" + str(n)]))
+			self.water_scales[n].set_value(attrs["control_parameter" + str(n)])
 			self.water_scales[n].set_hexpand(True)
 			self.water_scales[n].set_vexpand(True)
 			self.water_scales[n].connect("value-changed" , lambda scale, g=n : self.tasks.append(self.loop.create_task(self.update_water_threshold(g,scale.get_value()))))#This line of code is the answer to a specific engineering question: how many obscure features of Python can fit in one line of code? It also makes the slider schedule a task when you move it, so that it neither blocks the GUI nor fails to do anything.
 			self.water_pages[n].set_center_widget(self.water_scales[n])
 			self.deadband_scales.append(Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL,0,1,0.01))
-			self.deadband_scales[n].set_value(float(attrs["deadband" + str(n)]))
+			self.deadband_scales[n].set_value(attrs["deadband" + str(n)])
 			self.deadband_scales[n].set_hexpand(True)
 			self.deadband_scales[n].set_vexpand(True)
 			self.deadband_scales[n].connect("value-changed" , lambda scale, g=n : self.tasks.append(self.loop.create_task(self.update_water_deadband(g,scale.get_value()))))
@@ -343,7 +353,7 @@ class GUI:
 			self.light_pages[n].append(Gtk.Label(label="This is a test of the light control interface."))
 			self.light_pages[n].append(Gtk.Label(label="This will eventually display an indicator of if the light is running and a slider to control hours."))
 			self.light_scales.append(Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL,0,1,0.01))
-			self.light_scales[n].set_value(float(attrs["light_length" + str(n)]))
+			self.light_scales[n].set_value(attrs["light_length" + str(n)])
 			self.light_scales[n].set_hexpand(True)
 			self.light_scales[n].set_vexpand(True)
 			self.light_scales[n].connect("value-changed" , lambda scale, g=n : self.tasks.append(self.loop.create_task(self.update_light_length(g,scale.get_value()))))
@@ -378,8 +388,8 @@ class GUI:
 	async def toggle_recording(self,whermst):
 		global attrs
 		await self.lock.acquire()
-		attrs["recording_status"] = str(whermst)
-		set_attributes()
+		attrs["recording_status"] = whermst
+		attrs.sync()
 		self.lock.release()
 	async def force_capture(self):
 		global attrs
@@ -390,22 +400,23 @@ class GUI:
 	async def update_water_threshold(self,n,value):
 		global attrs
 		await self.lock.acquire()
-		attrs["control_parameter" + str(n)] = str(value)
-		set_attributes()
+		attrs["control_parameter" + str(n)] = value
+		attrs.sync()
 		self.lock.release()
 	async def update_water_deadband(self,n,value):
 		global attrs
 		await self.lock.acquire()
-		attrs["deadband" + str(n)] = str(value)
-		set_attributes()
+		attrs["deadband" + str(n)] = value
+		attrs.sync()
 		self.lock.release()
 	async def update_light_length(self,n,value):
 		global attrs
 		await self.lock.acquire()
-		attrs["light_length" + str(n)] = str(value)
-		set_attributes()
+		attrs["light_length" + str(n)] = value
+		attrs.sync()
 		self.lock.release()
 	async def update_settings(self):
+		assert False#This is BROKEN until we finish migrating to shelves
 		global attrs
 		await self.lock.acquire()
 		row = self.settings_listbox.get_selected_row()
@@ -453,24 +464,24 @@ class GUI:
 			light()
 			self.lock.release()
 			await self.update_GUI()
-			await asyncio.sleep(float(attrs["interval"]))
+			await asyncio.sleep(attrs["interval"])
 	async def camera_control(self):
 		global attrs
 		while True:
 			await self.lock.acquire()
-			if attrs["recording_status"] == True:
+			if attrs["recording_status"]:
 				camera_capture()
 			await self.update_GUI()
 			self.lock.release()
-			await asyncio.sleep(float(attrs["camera_interval"]))
+			await asyncio.sleep(attrs["camera_interval"])
 	async def update_GUI(self):
 		global attrs
-		for n in range(int(attrs["beds"])):
-			if (attrs["bed" + str(n)] == "True"):
+		for n in range(attrs["beds"]):
+			if attrs["bed" + str(n)]:
 				self.water_pages[n].get_start_widget().set_label("Bed " + str(n) + " is running.")
 			else:
 				self.water_pages[n].get_start_widget().set_label("Bed " + str(n) + " is not running.")
-		self.preview_image.set_from_file(get_file_name(int(attrs["last_file_number"])))
+		self.preview_image.set_from_file(get_file_name(attrs["last_file_number"]))
 		self.camera_text.set_label("Overall, " + attrs["last_file_number"] + " images have been captured by this device.\nCurrently, images will be captured every " + attrs["last_file_number"] + " seconds.")
 		return None
 
@@ -479,12 +490,3 @@ if attrs["is_debug"] == "True":
 	print(__name__)
 if __name__ == "__main__":
 	app()
-
-
-
-
-
-
-
-
-
